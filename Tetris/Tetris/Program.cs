b204223583791c1331ed31174a70610dedd07ede
kartavec <kartavec@gmail.com>
2019-11-27 20:57:31 +0300

    Создали проект Hello world

---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
