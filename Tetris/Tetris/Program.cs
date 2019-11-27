211e8bfde98de5e35bb89a237143bdcff59f574e
kartavec <kartavec@gmail.com>
2019-11-27 20:58:02 +0300

    Добавил !!

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,7 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
