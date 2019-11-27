884f51da09110301159479c5409f7c8da7f88148
kartavec <kartavec@gmail.com>
2019-11-27 21:41:41 +0300

    Добавлен класс Point

---
+++ Tetris/Tetris/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char c;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -9,26 +9,21 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            int x1 = 2;
-            int y1 = 3;
-            char c1 = '*';
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
 
-            Draw(x1, y1, c1);
-
-            int x2 = 2;
-            int y2 = 3;
-            char c2 = '#';
-
-            Draw(x2, y2, c2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.c = '*';
+            p2.Draw();
 
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
-        }
     }
 }
