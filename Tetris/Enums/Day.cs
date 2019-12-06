9046051dfba3909814c4f83cd18eb7f51e138b4f
kartavec <kartavec@gmail.com>
2019-12-06 20:35:22 +0300

    Enum Day

---
+++ Tetris/Enums/Day.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enums
+{
+    enum Day : byte
+    {
+        Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun
+    }
+}
---
+++ Tetris/Enums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = Day.Mon;
+            int i = 3;
+
+            Day day3 = (Day)i;
+            Console.WriteLine(day3);
+
+
+            if(day == Day.Mon)
+            {
+                Console.WriteLine("Понедельник");
+                Console.WriteLine((int)day);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
