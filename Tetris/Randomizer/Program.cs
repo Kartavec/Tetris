55da22daaa4fa1135319330e6af5393e21e56b5c
kartavec <kartavec@gmail.com>
2019-12-13 17:52:54 +0300

    Проект Randomizer, класс Random

---
+++ Tetris/Randomizer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            
+
+            for(int i = 0; i < 20; i++)
+            {
+                int value = rand.Next(0, 5);
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
