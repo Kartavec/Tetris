b2a5a495bb9a9fefa8507aca95245fc3613eace6
kartavec <kartavec@gmail.com>
2019-12-13 18:32:17 +0300

    Реализовали падение фигурок

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,62 +11,27 @@ namespace Tetris
             Console.SetBufferSize(40, 30);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure s = generator.GetNewFigure();
-            
-            s.Draw();
+            Figure s = null;
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            while (true)
+            {
+                FigureFall(s, generator);
+                s.Draw();
+            }
+        }
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.LEFT);
-            s.Draw();
+        static void FigureFall(Figure fig, FigureGenerator generator)
+        {
+            fig = generator.GetNewFigure();
+            fig.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.RIGHT);
-            s.Draw();
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
-
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.RIGHT);
-            s.Draw();
-            //Stick stick = new Stick(6, 6, '*');
-            //stick.Draw();
-
-            //Point p1 = new Point(2, 3, '*');
-            //p1.Draw();
-
-            //Point p2 = new Point()
-            //{
-            //    x = 4,
-            //    y = 5,
-            //    c = '#'
-            //};
-
-            //p2.Draw();
-
-
-            Console.ReadLine();
+            for (int i = 0; i < 15; i++)
+            {
+                fig.Hide();
+                fig.Move(Direction.DOWN);
+                fig.Draw();
+                Thread.Sleep(200);
+            }
         }
 
     }
