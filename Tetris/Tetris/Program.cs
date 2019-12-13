aaae034c79b114860525ebb739d438150d92a4fa
kartavec <kartavec@gmail.com>
2019-12-13 19:08:10 +0300

    Использование ref и out

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,16 +11,16 @@ namespace Tetris
             Console.SetBufferSize(40, 30);
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure s = null;
+            Figure s;
 
             while (true)
             {
-                FigureFall(s, generator);
+                FigureFall(out s, generator);
                 s.Draw();
             }
         }
 
-        static void FigureFall(Figure fig, FigureGenerator generator)
+        static void FigureFall(out Figure fig, FigureGenerator generator)
         {
             fig = generator.GetNewFigure();
             fig.Draw();
