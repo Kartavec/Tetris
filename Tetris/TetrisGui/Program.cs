7e572ef7bc4bb3f446a79be0ef4b387b4b9f4038
kartavec <kartavec@gmail.com>
2020-01-25 16:31:56 +0300

    Использование библиотеки SmallBasic

---
+++ Tetris/TetrisGui/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TetrisGui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphicsWindow.Width = 100;
+            GraphicsWindow.Height = 100;
+
+            GraphicsWindow.BackgroundColor = "LightBlue";
+
+            GraphicsWindow.PenColor = "Red";
+            GraphicsWindow.DrawRectangle(20, 20, 10, 10);
+        }
+    }
+}
